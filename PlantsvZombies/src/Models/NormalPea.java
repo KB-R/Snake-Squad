db6f4ef8041c90e22d6bd3788bc6726656917d27
KB-R <kburtonrowe@gmail.com>
2018-10-27 10:54:29 -0400

    Update NormalPea.java

--- PlantsvZombies/src/Models/NormalPea.java
+++ PlantsvZombies/src/Models/NormalPea.java
@@ -1,42 +1,41 @@
 package Models;
 /**
- * 
+ * The normal pea is the object that
  * @author Kurt Burton-Rowe
  * @date October 26,2018
  * @version 1.0
- *
- * The normal pea is the object that 
  */
 public class NormalPea extends NPC implements Movable{
-	private int x, y;
 	private int speed;
-	private boolean contact = false;
+	private final int damage = 10;
+	private boolean contact = false; //boolean could be useful for the collision detector
 	
 	public NormalPea(int nX, int nY) {
 		super(0, true);
 		setVelocity(4);
-		this.x = arry[0];
-		this.y = arry[1];
+		this.x = coordinates[0];
+		this.y = coordinates[1];
 		this.setLocation(nX, nY);
 	}
 	
 	@Override
 	public int getVelocity() {
-		// TODO Auto-generated method stub
 		return speed;
 	}
 	/*Sets the speed of the pea's traversal through the board */
 	@Override
 	public void setVelocity(int newV) {
-		// TODO Auto-generated method stub
 		speed = newV;
 	}
 	/*Moves the pea through the board only the x coordinate changes*/
 	@Override
 	public void move() {
-		// TODO Auto-generated method stub
-		x-=2;
+		x+=4;
 		this.setLocation(x, y);
+	}
+
+	public int getDamage() {
+		return damage;
 	}
 	
 }
