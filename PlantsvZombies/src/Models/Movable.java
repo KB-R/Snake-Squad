7f15cdef2ef319c46ffaa01e526b34e2f1228c38
KB-R <kburtonrowe@gmail.com>
2018-10-27 10:59:20 -0400

    Update Movable.java

--- PlantsvZombies/src/Models/Movable.java
+++ PlantsvZombies/src/Models/Movable.java
@@ -1,27 +1,22 @@
 package Models;
 
 /**
+ * All movable objects must implement this interface (Peas, Zombies and Lawn mowers)
  * @author Maxime Ndutiye 
  * @version 1.0
  * @date October 25, 2018
- * All movable objects must implement this interface (Peas, Zombies and Lawn mowers)
  */
 public interface Movable{
     public int velocity = 1;
 
     /**
-     * get the objects velocity
-     * @return int the velocity
+     * @return get the objects velocity
      */
     public int getVelocity();
 
-    /**
-     * Set the velocity
-     */
+    /* Set the velocity */
     public void setVelocity(int newV);
 
-    /**
-     * Move
-     */
+    /* Move the object */
     public void move();
 }
