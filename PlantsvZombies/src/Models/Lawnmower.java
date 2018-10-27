a9a45e8bf6f6be5d173867860f34b4803fcfadd1
KB-R <kburtonrowe@gmail.com>
2018-10-27 11:04:42 -0400

    Update Lawnmower.java

--- PlantsvZombies/src/Models/Lawnmower.java
+++ PlantsvZombies/src/Models/Lawnmower.java
@@ -1,24 +1,24 @@
 package Models;
 /**
- * 
+ * The lawn mower is the indestructible get out of jail free card. It destroys all enemies in its row ONCE.
  * @author Kurt Burton-Rowe
  * @version 1.0
  * @date October 26, 2018
- * The lawn mower is the indestructible get out of jail free card. It destroys all enemies in its row ONCE. 
  */
 public class Lawnmower extends NPC implements Movable{
-	private static int maxHealth = 10000000;
-	private static int damage = 10000000;
-	private static int speed = 9;
-	private int x, y;
-	private boolean contact = false;
+	private final static int maxHealth = 10000000;
+	private final int damage = 10000000;
 	
-	public Lawnmower(){
+
+	private final int speed = 9;
+	private int x, y;
+	
+	public Lawnmower(int x, int y){
 		super(maxHealth, true);
 		this.setVelocity(speed);
-		this.x = arry[0];
-		this.y = arry[1];
-		this.setLocation(x,y);
+		this.x = x;
+		this.y = y;
+		this.setLocation(this.x,this.y);
 	}
 	
 	/*When the Lawn mower moves it goes to the end of the board only after it gets hit by a zombie*/
@@ -27,21 +27,25 @@ public class Lawnmower extends NPC implements Movable{
 			this.move();
 		}
 	}
+	/**
+	 * @return The speed that the lawn mower traverses the board
+	 */
 	@Override
 	public int getVelocity() {
-		// TODO Auto-generated method stub
 		return speed;
 	}
+	
+	/*Cannot change the speed of the lawn mower*/ 
 	@Override
-	public void setVelocity(int newV) {
-		// TODO Auto-generated method stub
-		this.speed = newV;
+	public void setVelocity(int newV) {}
+	
+	public int getDamage() {
+		return damage;
 	}
 	
 	/*When the Lawn mower moves it goes to the end of the board only after it gets hit by a zombie*/
 	@Override
 	public void move() {
-		// TODO Auto-generated method stub
 		x+=9; //
 		this.setLocation(this.x, this.y);
 	}
